6baf12fb602cb94e85e8f3c7d1b007c404d882f8
Matheus <msouzagaspar@gmail.com>
2021-02-23 14:59:34 -0300

    first commit

---
+++ src/WorldPopulation/CsvReader.cs
@@ -0,0 +1,45 @@
+using System.IO;
+
+namespace WorldPopulation
+{
+  class CsvReader
+  {
+    private string _csvFilePath;
+
+    public CsvReader(string csvFilePath)
+    {
+      _csvFilePath = csvFilePath;
+    }
+
+    public Country[] ReadFirstNCountries(int nCountries)
+    {
+      Country[] countries = new Country[nCountries];
+
+      using (StreamReader sr = new StreamReader(_csvFilePath))
+      {
+        // read header line
+        sr.ReadLine();
+
+        for (int i = 0; i < nCountries; i++)
+        {
+          string csvLine = sr.ReadLine();
+          countries[i] = ReadCountryFromCsvLine(csvLine);
+        }
+      }
+
+      return countries;
+    }
+
+    public Country ReadCountryFromCsvLine(string csvLine)
+    {
+      string[] parts = csvLine.Split(new char[] { ',' });
+
+      string name = parts[0];
+      string code = parts[1];
+      string region = parts[2];
+      int population = int.Parse(parts[3]);
+
+      return new Country(name, code, region, population);
+    }
+  }
+}
---
+++ src/WorldPopulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WorldPopulation
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string filePath = @"../../population-data.csv";
+      CsvReader reader = new CsvReader(filePath);
+
+      Country[] countries = reader.ReadFirstNCountries(10);
+
+      foreach (Country country in countries)
+      {
+        Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+      }
+    }
+  }
+}
