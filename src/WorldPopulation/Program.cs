325581976e7c3cc3200e6b6f9f4f0801e9cdb880
Matheus <msouzagaspar@gmail.com>
2021-02-24 08:51:57 -0300

    refactor get maxToDisplay user input

--- src/WorldPopulation/Program.cs
+++ src/WorldPopulation/Program.cs
@@ -12,7 +12,8 @@ namespace WorldPopulation
       Console.WriteLine("Welcome! Please choose one of the options, or type anything else to quit.");
       Console.WriteLine("1. List the n most populous countries in the list");
       Console.WriteLine("2. List all countries in the list");
-      Console.WriteLine("3. Find country population by country code");
+      Console.WriteLine("3. List all countries in reverse order");
+      Console.WriteLine("4. Find country population by country code");
 
       string input = Console.ReadLine();
       Console.WriteLine("");
@@ -28,6 +29,10 @@ namespace WorldPopulation
           break;
 
         case "3":
+          GetAllCountriesInReverseOrder(filePath);
+          break;
+
+        case "4":
           GetCountryByCode(filePath);
           break;
 
@@ -59,18 +64,7 @@ namespace WorldPopulation
       countries.RemoveAt(lilliputIndex);
       //
 
-      int userInput;
-      while (true)
-      {
-        Console.Write("Enter the number of countries to display: ");
-        bool inputIsInt = int.TryParse(Console.ReadLine(), out userInput);
-        if (!inputIsInt || userInput <= 0)
-          Console.WriteLine("You must type in a positive integer.");
-        else
-          break;
-      }
-
-      int maxToDisplay = userInput;
+      int maxToDisplay = AskForMaxToDisplay();
 
       for (int i = 0; i < countries.Count; i++)
       {
@@ -82,7 +76,28 @@ namespace WorldPopulation
         }
 
         Country country = countries[i];
-        Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+        Console.WriteLine($"{i + 1}: {PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+      }
+    }
+
+    private static void GetAllCountriesInReverseOrder(string filePath)
+    {
+      CsvReader reader = new CsvReader(filePath);
+      List<Country> countries = reader.ReadAllCountriesToList();
+
+      int maxToDisplay = AskForMaxToDisplay();
+
+      for (int i = 0; i < countries.Count; i++)
+      {
+        if (i > 0 && (i % maxToDisplay == 0))
+        {
+          Console.WriteLine("Hit return to continue, or anything else to quit");
+          if (Console.ReadLine() != "")
+            break;
+        }
+
+        Country country = countries[i];
+        Console.WriteLine($"{i + 1}: {PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
       }
     }
 
@@ -100,5 +115,21 @@ namespace WorldPopulation
       else
         Console.WriteLine($"{country.Name} has population {PopulationFormatter.FormatPopulation(country.Population)}");
     }
+
+    private static int AskForMaxToDisplay()
+    {
+      int userInput;
+      while (true)
+      {
+        Console.Write("Enter the number of countries to display: ");
+        bool inputIsInt = int.TryParse(Console.ReadLine(), out userInput);
+        if (!inputIsInt || userInput <= 0)
+          Console.WriteLine("You must type in a positive integer.");
+        else
+          break;
+      }
+      return userInput;
+    }
   }
 }
+
